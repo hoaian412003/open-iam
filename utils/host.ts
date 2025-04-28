1ab4abd82bd4a8678c114129bf4411e0898dcb23
Martin <tran.hoai.an@greenskies.com>
2025-04-28 22:34:23 +0700

    Update host

--- utils/host.ts
+++ utils/host.ts
@@ -1,2 +1,2 @@
-export const host = process.env.VERCEL_URL || 'http://localhost:3000';
+export const host = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
 console.log("Host is: ", host)
