9b3258ecb0a9da44f95c215d740bee0ddc5b5995
Martin <tran.hoai.an@greenskies.com>
2025-05-02 15:13:10 +0700

    Update work flow

--- scripts/define-cypress-tags.sh
+++ scripts/define-cypress-tags.sh
@@ -3,9 +3,10 @@
 read -a labels <<<$(./scripts/find-trello-card.sh $1 $2 $3 | jq -r '.labels[].name')
 # Labels is array of string with prefix is @
 
-tags=$(
-  IFS='+'
-  echo "${labels[*]}"
-)
+result=""
 
-echo "$tags"
+for i in "${!labels[@]}"; do
+  result+=${labels[i]}
+done
+
+printf "%s+" "${labels[@]}"
