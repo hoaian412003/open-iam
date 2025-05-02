64d94d04c8a78b264c29aae28728d05c9a8e2114
Martin <tran.hoai.an@greenskies.com>
2025-05-02 14:38:36 +0700

    Update work flow

--- scripts/prepare-github-context.sh
+++ scripts/prepare-github-context.sh
@@ -3,4 +3,5 @@
 branch="${GITHUB_REF_NAME#ft/}"
 
 echo "TRELLO_CARD_ID=$branch" >>$GITHUB_ENV
+echo $(./scripts/define-cypress-tags.sh $branch $TRELLO_KEY $TRELLO_TOKEN)
 echo "CYPRESS_grepTags=$(./scripts/define-cypress-tags.sh $branch $TRELLO_KEY $TRELLO_TOKEN)" >>$GITHUB_ENV
