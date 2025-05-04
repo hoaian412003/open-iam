7f1eae5a73a11049fe54f432e98742f27405d871
Martin <tran.hoai.an@greenskies.com>
2025-05-04 12:48:29 +0700

    Update work flow

--- scripts/define-cypress-tags.sh
+++ scripts/define-cypress-tags.sh
@@ -3,10 +3,9 @@
 read -a labels <<<$(./scripts/find-trello-card.sh $1 $2 $3 | jq -r '.labels[].name')
 # Labels is array of string with prefix is @
 
-result=""
+tags=$(
+  IFS='+'
+  echo "${labels[*]}"
+)
 
-for i in "${!labels[@]}"; do
-  result+=${labels[i]}
-done
-
-printf "%s+" "${labels[@]}"
+echo "$tags"
--- scripts/prepare-trello-context.sh
+++ scripts/prepare-trello-context.sh
@@ -4,4 +4,4 @@ branch="${GITHUB_REF_NAME#ft/}"
 
 echo "TRELLO_CARD_ID=$branch" >>$GITHUB_ENV
 echo $(./scripts/define-cypress-tags.sh $branch $TRELLO_KEY $TRELLO_TOKEN)
-echo "CYPRESS_grepTags=$(./scripts/define-cypress-tags.sh $branch $TRELLO_KEY $TRELLO_TOKEN)" >>$GITHUB_ENV
+echo "TRELLO_TAGS=$(./scripts/define-cypress-tags.sh $branch $TRELLO_KEY $TRELLO_TOKEN)" >>$GITHUB_ENV
